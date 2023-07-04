a68d124cdde9a8dddf9226e1ac79abf83c3a7d0c
KvRae <karamelmannai@gmail.com>
2023-07-04 20:17:00 +0100

    working with topbar + improvments on signin

--- app/src/main/java/slikoo/kvrae/slikoo/ui/pages/SplashScreen.kt
+++ app/src/main/java/slikoo/kvrae/slikoo/ui/pages/SplashScreen.kt
@@ -1,7 +1,5 @@
 package slikoo.kvrae.slikoo.ui.pages
 
-
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
@@ -16,14 +14,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import slikoo.kvrae.slikoo.R
 
 @Composable
-fun AnimatedSplashScreen() {
+fun AnimatedSplashScreen(navController: NavController) {
     val startAnimation by remember {
         mutableStateOf(false)
     }
@@ -32,25 +31,27 @@ fun AnimatedSplashScreen() {
             durationMillis = 3000
         )
     )
+        Box(
+            modifier = Modifier
+                .background(
+                    if (isSystemInDarkTheme()) Color.Black else Color.Red
+                )
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.logo2), // Load the drawable resource
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .size(200.dp)
+                    .alpha(alphaAnim.value)
+            )
+        }
+    if (startAnimation) {
+        onSplashScreenFinished(navController)
+    }
 }
 
-@Preview(uiMode = UI_MODE_NIGHT_YES)
-@Composable
-fun splash() {
-    val logo = if (isSystemInDarkTheme()) R.drawable.logo2 else R.drawable.logo2
-    Box(
-        modifier = Modifier
-            .background(
-                if (isSystemInDarkTheme()) Color.Black else Color.Red
-            )
-            .fillMaxSize(),
-    contentAlignment = Alignment.Center
-    ){
-        Image(
-            painter = painterResource(id = logo), // Load the drawable resource
-            contentDescription = "Logo",
-            modifier = Modifier.size(200.dp)
-        )
-    }
-
+fun onSplashScreenFinished(navController: NavController) {
+    navController.navigate("login")
 }
